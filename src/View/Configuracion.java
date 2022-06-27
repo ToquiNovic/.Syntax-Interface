b0e81b2ae4e20ceaf15bfc02fc2c9e2b55b76a1b
Toqui <j.toquica@udla.edu.co>
2022-06-27 07:35:58 -0500

    Primer commit

---
+++ src/View/Configuracion.java
@@ -0,0 +1,28 @@
+
+package View;
+
+public class Configuracion extends javax.swing.JPanel {
+
+ 
+    public Configuracion() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        Fondo = new javax.swing.JLabel();
+
+        setPreferredSize(new java.awt.Dimension(840, 470));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/Fondo.jpg"))); // NOI18N
+        add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(4, 0, 890, 470));
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Fondo;
+    // End of variables declaration//GEN-END:variables
+}
